aa8ba9401b2f66732683e4984e9e75c9aabdc3a5
Shane Becker <veganstraightedge@gmail.com>
2018-07-05 12:09:08 -0700

    Add new style migration timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516194159) do
+ActiveRecord::Schema.define(version: 2017_05_16_194159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
