77279039a9aba0f69c3dc6c1db108d4e10ea9d3c
Shane Becker <veganstraightedge@gmail.com>
2018-07-05 17:26:31 -0700

    Rubocop fix: RSpec/InstanceVariable

--- spec/views/submissions/edit.html.erb_spec.rb
+++ spec/views/submissions/edit.html.erb_spec.rb
@@ -2,19 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'submissions/edit', type: :view do
   before do
-    @submission = assign(:submission, Submission.create!(
-                                        url: 'MyText',
-                                        html: 'MyText',
-                                        base_url: 'MyText',
-                                        save_html: false,
-                                        render_html_in_page: false
-                                      ))
+    let(:submission) do
+      assign(
+        :submission,
+        Submission.create!(
+          url: 'MyText',
+          html: 'MyText',
+          base_url: 'MyText',
+          save_html: false,
+          render_html_in_page: false
+        )
+      )
+    end
   end
 
   it 'renders the edit submission form' do
     render
 
-    assert_select 'form[action=?][method=?]', submission_path(@submission), 'post' do
+    assert_select 'form[action=?][method=?]', submission_path(submission), 'post' do
       assert_select 'textarea[name=?]', 'submission[url]'
 
       assert_select 'textarea[name=?]', 'submission[html]'
