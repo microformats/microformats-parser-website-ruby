76f017f6df6b57e22316c580f6b8ee1b08ff6713
Jason Garber <jason@sixtwothree.org>
2020-04-20 16:58:40 -0400

    Fix deprecation warnings

--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :submission do
-    url 'MyText'
-    html 'MyText'
-    base_url 'MyText'
-    save_html false
-    render_html_in_page false
+    url { 'MyText' }
+    html { 'MyText' }
+    base_url { 'MyText' }
+    save_html { false }
+    render_html_in_page { false }
   end
 end
